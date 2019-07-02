6f89aa7aec6c5f5ae18e7ff4d4d0900039aaf5ef
AlfredBryan <alfred.chimereze@gmail.com>
2019-07-02 13:45:26 +0100

    fixes

--- src/components/Assessment/takeAssessment.js
+++ src/components/Assessment/takeAssessment.js
@@ -21,7 +21,8 @@ class takeAssessment extends Component {
       assessment_id: this.props.match.params.id,
       questions: [],
       currentPage: 1,
-      answer: ""
+      possible_selections: [],
+      selections: {}
     };
 
     this.converter = new Showdown.Converter({
@@ -32,12 +33,87 @@ class takeAssessment extends Component {
     });
   }
 
-  getInitialAnswerState = question => {
-    return { selectedValue: "apple" };
+  sendTheoryAnswer = (value, question_id) => {
+    const token = localStorage.getItem("token");
+    const { selections, assessment_id } = this.state;
+
+    axios
+      .post(
+        `https://smart-up.herokuapp.com/api/v1/assessments/${assessment_id}/answer`,
+        {
+          answer: {
+            assessment_id: assessment_id,
+            question_id: question_id,
+            content: value
+          }
+        },
+        {
+          headers: {
+            Authorization: token
+          }
+        }
+      )
+      .then(res => {
+        selections[res.data.question_id] = res.data.content;
+        this.setState({
+          selections: selections
+        });
+      })
+      .catch(error => {});
+  };
+
+  sendAnswer = option => {
+    const token = localStorage.getItem("token");
+    const { selections, assessment_id } = this.state;
+    axios
+      .post(
+        `https://smart-up.herokuapp.com/api/v1/assessments/${assessment_id}/answer`,
+        {
+          answer: {
+            assessment_id: assessment_id,
+            question_id: option.question_id,
+            answer_option_id: option.id
+          }
+        },
+        {
+          headers: {
+            Authorization: token
+          }
+        }
+      )
+      .then(res => {
+        selections[res.data.question_id] = res.data.answer_option;
+        this.setState({
+          selections: selections
+        });
+      })
+      .catch(error => {});
+  };
+
+  finishAssessment = () => {
+    const token = localStorage.getItem("token");
+    const { assessment_id } = this.state;
+    axios
+      .post(
+        `https://smart-up.herokuapp.com/api/v1/assessments/${assessment_id}/finish`,
+        {
+          headers: {
+            Authorization: token
+          }
+        }
+      )
+      .then(res => {
+        this.props.history.replace("/profile");
+      })
+      .catch(error => {});
+  };
+  Capitalize = str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
   fetchAssessment = () => {
     const token = localStorage.getItem("token");
+    const { possible_selections } = this.state;
     const ReactMarkdown = require("react-markdown");
 
     axios
@@ -52,50 +128,91 @@ class takeAssessment extends Component {
         }
       )
       .then(res => {
-        const { answer } = this.state;
+        res.data.questions.map(question => {
+          possible_selections.push(question.answer_options);
+        });
+
         const result = res.data.questions.map(
-          ({ id, name, description, answer_options, question_type }) => (
+          ({ id, name, description, answer_options, question_type }, index) => (
             <div key={id} className="assessment_question">
-              <h3>{name}</h3>
+              <h3>
+                Question <span>{index + 1}</span> of{" "}
+                <span>{res.data.questions.length}</span>
+              </h3>
+              <br />
+              <h4>{this.Capitalize(name)}</h4>
               <blockquote>
                 <ReactMarkdown source={description} />
               </blockquote>
-              {question_type === "choice" ? (
-                <RadioGroup
-                  name="selected_option"
-                  selectedValue={this.state.selectedValue}
-                  onChange={this.handleAnswerSelect}
-                >
-                  {answer_options.map(item => (
-                    <div key={item.id} className="col-md-6">
-                      <div className="card">
-                        <label key={item.id} className="answer_option">
-                          <Radio value={item.content} />
-                          {item.content}
-                        </label>
-                      </div>
-                    </div>
-                  ))}
-                </RadioGroup>
-              ) : (
-                <div className="col-lg-8">
-                  <ReactMde
-                    onChange={this.handleAnswerChange}
-                    value={answer}
-                    generateMarkdownPreview={markdown =>
-                      Promise.resolve(this.converter.makeHtml(markdown))
-                    }
-                  />
-                </div>
-              )}
+
+              {this.prepareAnswerSpace(answer_options, question_type, id)}
             </div>
           )
         );
         this.setState({
-          questions: result
+          questions: result,
+          possible_selections: possible_selections.flat()
         });
       })
       .catch(error => {});
+  };
+
+  fetchUserAnswers = () => {
+    const token = localStorage.getItem("token");
+    const user = JSON.parse(localStorage.getItem("user"));
+    const { selections, assessment_id } = this.state;
+
+    axios
+      .get(
+        `https://smart-up.herokuapp.com/api/v1/assessments/${assessment_id}/answers?user_id=${
+          user.id
+        }`,
+        {
+          headers: {
+            Authorization: token
+          }
+        }
+      )
+      .then(res => {
+        res.data.map(answer => {
+          selections[answer.question.id] =
+            answer.question.question_type === "choice"
+              ? answer.answer_option
+              : answer.content;
+        });
+        this.setState({
+          selections: selections
+        });
+      })
+      .catch(error => {});
+  };
+
+  searchArray(value, array) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]["id"] === value) {
+        return array[i];
+      }
+    }
+  }
+
+  getKeyByValue(object, value) {
+    return Object.keys(object).find(
+      key => this.searchArray(value, object[key])["id"] === value
+    );
+  }
+
+  currentTheoryAnswer = id => {
+    const { selections } = this.state;
+    if (selections[id]) {
+      return selections[id];
+    }
+  };
+
+  realSelectedValue = id => {
+    const { selections } = this.state;
+    if (selections[id]) {
+      return selections[id]["id"];
+    }
   };
 
   handlePageChange = page => {
@@ -104,13 +221,64 @@ class takeAssessment extends Component {
     });
   };
 
-  handleAnswerChange = answer => {
-    this.setState({ answer });
+  handleAnswerSelect = id => {
+    const { possible_selections } = this.state;
+    let answer_option = this.searchArray(id, possible_selections);
+    this.sendAnswer(answer_option);
   };
 
-  handleAnswerSelect = () => {};
+  prepareAnswerSpace = (answer_options, question_type, id) => {
+    let elements;
+    let answer_text = this.currentTheoryAnswer(id);
+
+    if (question_type === "theory") {
+      elements = (
+        <div className="form-group">
+          <label className="col-lg-8 adjust-input control-label">Content</label>
+          <div className="col-lg-12">
+            <ReactMde
+              onChange={value => this.sendTheoryAnswer(value, id)}
+              value={answer_text}
+              generateMarkdownPreview={markdown =>
+                Promise.resolve(this.converter.makeHtml(markdown))
+              }
+            />
+          </div>
+        </div>
+      );
+    } else {
+      elements = (
+        <RadioGroup
+          key={id}
+          name="selected_option"
+          selectedValue={this.realSelectedValue(id)}
+          onChange={this.handleAnswerSelect}
+        >
+          {answer_options.map(item => (
+            <div key={item.id} className="col-md-6">
+              <div
+                className={
+                  this.realSelectedValue(id) === item.id
+                    ? "card selected"
+                    : "card"
+                }
+                id={`card_${item.id}`}
+              >
+                <label key={item.id} className="answer_option">
+                  <Radio value={item.id} />
+                  {item.content}
+                </label>
+              </div>
+            </div>
+          ))}
+        </RadioGroup>
+      );
+    }
+    return elements;
+  };
 
   componentDidMount() {
+    this.fetchUserAnswers();
     this.fetchAssessment();
   }
 
@@ -127,6 +295,7 @@ class takeAssessment extends Component {
         <div className="main-content">
           <div className="container" id="take_assessment">
             <ul>{questions[currentPage - 1]}</ul>
+            <div className="clearfix" />
             <Pagination
               total={total}
               limit={limit}
@@ -198,6 +367,7 @@ class takeAssessment extends Component {
 
                   {!hasNextPage && (
                     <Button
+                      onClick={this.finishAssessment}
                       variant="contained"
                       component="span"
                       color="primary"
